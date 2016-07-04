d100b834c85092ac9a8fbeaa993579033d9bb578
Alisson Chiquitto <chiquitto@gmail.com>
2016-07-04 00:00:19 -0300

    Cidade Delete

--- src/java/mb/CidadesMb.java
+++ src/java/mb/CidadesMb.java
@@ -4,11 +4,19 @@ import br.com.chiquitto.aula.jdbcescola.dao.CidadeDao;
 import br.com.chiquitto.aula.jdbcescola.vo.Cidade;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 @ManagedBean
+@ViewScoped
 public class CidadesMb {
 
     private List<Cidade> cidades;
+    
+    public void delete(Cidade cidade) {
+        CidadeDao cidadeDao = new CidadeDao();
+        cidadeDao.apagar(cidade);
+        this.cidades = null;
+    }
 
     public List<Cidade> getCidades() {
         if (this.cidades == null) {
