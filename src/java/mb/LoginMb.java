cff0d7959fc7e9ccea6db6cbb2ffce3554732f85
Alisson Chiquitto <chiquitto@gmail.com>
2016-07-09 00:16:18 -0300

    Login

---
+++ src/java/mb/LoginMb.java
@@ -0,0 +1,52 @@
+package mb;
+
+import br.com.chiquitto.aula.jdbcescola.dao.UsuarioDao;
+import br.com.chiquitto.aula.jdbcescola.exception.RowNotFoundException;
+import br.com.chiquitto.aula.jdbcescola.vo.Usuario;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+public class LoginMb {
+    private String email;
+    private String senha;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    public String login() {
+        UsuarioDao dao = new UsuarioDao();
+        
+        Usuario usuario = new Usuario();
+        usuario.setEmail(email);
+        usuario.setSenha(senha);
+        
+        try {
+            usuario = dao.getByEmailSenha(usuario);
+            
+            FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("usuario", usuario);
+            
+            return "index";
+        } catch (RowNotFoundException ex) {
+            // informacoes incorretas
+            FacesMessage msg = new FacesMessage("Email e/ou Senha incorretos");
+            FacesContext.getCurrentInstance().addMessage(null, msg);
+        }
+        
+        return null;
+    }
+}
